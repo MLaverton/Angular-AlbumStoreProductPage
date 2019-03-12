0656ea446049877138012aea23e3a5533e6a69ae
MLaverton <33700030+MLaverton@users.noreply.github.com>
2019-03-12 16:26:48 +0000

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -15,6 +15,6 @@ private _albumUrl = '../assets/album.json';
   }
 
   getProducts(): Observable<Product[]>{
-    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json();
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
   }
 }
