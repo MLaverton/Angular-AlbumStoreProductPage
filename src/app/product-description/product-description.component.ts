66194b2324e963c7d543aa107f24bbdb24ba9636
MLaverton <33700030+MLaverton@users.noreply.github.com>
2019-03-12 15:19:11 +0000

    Module 3

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import '../product.service';
+import {ProductService} from '../product.service';
 
 @Component({
   selector: 'app-product-description',
